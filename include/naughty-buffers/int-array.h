dce75f6422f45c0ec34db724d2c98b3bc2bbcbe2
Leonardo <leonardo_freitas_ext@carrefour.com>
2023-09-27 10:23:26 -0300

    remove int-array

--- include/naughty-buffers/int-array.h
+++
@@ -1,8 +0,0 @@
-#ifndef NAUGHTY_BUFFERS_INT_ARRAY_H
-#define NAUGHTY_BUFFERS_INT_ARRAY_H
-
-#include "naughty-buffers/array-generator.h"
-
-NAUGHTY_BUFFERS_ARRAY_DECLARATION(nb_int_array, int)
-
-#endif // NAUGHTY_BUFFERS_INT_ARRAY_H
--- src/naughty-buffers/int-array.c
+++
@@ -1,3 +0,0 @@
-#include "naughty-buffers/int-array.h"
-
-NAUGHTY_BUFFERS_ARRAY_DEFINITION(nb_int_array, int)
