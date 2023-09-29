02c0497ee938033ec2a11d8105bd83bdd1a07b10
Leonardo <leonardo_freitas_ext@carrefour.com>
2023-09-29 11:09:38 -0300

    drop criterion for insert test

--- src/tests/insert.c
+++ src/tests/insert.c
@@ -1,24 +1,23 @@
-#include "criterion/criterion.h"
 #include "naughty-buffers/buffer.h"
-#include <stdio.h>
+#include <assert.h>
 
-Test(insert, insert_increases_count_correctly) {
+void insert_increases_count_correctly() {
   struct nb_buffer buffer;
   uint32_t value = 0;
   nb_init(&buffer, sizeof(uint32_t));
-  cr_assert(buffer.block_count == 0);
+  assert(buffer.block_count == 0);
   nb_insert(&buffer, 0, &value);
-  cr_assert(buffer.block_count == 1);
+  assert(buffer.block_count == 1);
   nb_insert(&buffer, 1, &value);
-  cr_assert(buffer.block_count == 2);
+  assert(buffer.block_count == 2);
   nb_insert(&buffer, 2, &value);
-  cr_assert(buffer.block_count == 3);
+  assert(buffer.block_count == 3);
   nb_insert(&buffer, 9, &value);
-  cr_assert(buffer.block_count == 10);
+  assert(buffer.block_count == 10);
   nb_release(&buffer);
 }
 
-Test(insert, insert_stores_the_right_values) {
+void insert_stores_the_right_values() {
   struct nb_buffer buffer;
   uint32_t value = 0;
   uint32_t * read_value = NULL;
@@ -27,22 +26,22 @@ Test(insert, insert_stores_the_right_values) {
 
   nb_insert(&buffer, 0, &value);
   read_value = nb_at(&buffer, 0);
-  cr_assert(*read_value == 0);
+  assert(*read_value == 0);
 
   value = 3;
   nb_insert(&buffer, 0, &value);
   read_value = nb_at(&buffer, 0);
-  cr_assert(*read_value == 3);
+  assert(*read_value == 3);
 
   value = 33333;
   nb_insert(&buffer, 0, &value);
   read_value = nb_at(&buffer, 0);
-  cr_assert(*read_value == 33333);
+  assert(*read_value == 33333);
 
   nb_release(&buffer);
 }
 
-Test(insert, insert_store_values_not_addresses) {
+void insert_store_values_not_addresses() {
   struct nb_buffer buffer;
   uint32_t value = 0;
   uint32_t * read_value = NULL;
@@ -51,39 +50,39 @@ Test(insert, insert_store_values_not_addresses) {
 
   nb_insert(&buffer, 0, &value);
   read_value = nb_at(&buffer, 0);
-  cr_assert(read_value != &value);
+  assert(read_value != &value);
 
   value = 3;
   nb_insert(&buffer, 1, &value);
   read_value = nb_at(&buffer, 1);
-  cr_assert(read_value != &value);
+  assert(read_value != &value);
 
   value = 33333;
   nb_insert(&buffer, 2, &value);
   read_value = nb_at(&buffer, 2);
-  cr_assert(read_value != &value);
+  assert(read_value != &value);
 
   nb_release(&buffer);
 }
 
-Test(insert, insert_properly_stretches_the_buffer) {
+void insert_properly_stretches_the_buffer() {
   struct nb_buffer buffer;
   uint32_t value = 0;
 
   nb_init(&buffer, sizeof(uint32_t));
 
   nb_insert(&buffer, 33, &value);
-  cr_assert(buffer.block_count == 34);
-  cr_assert(buffer.block_capacity >= 33);
+  assert(buffer.block_count == 34);
+  assert(buffer.block_capacity >= 33);
 
   nb_insert(&buffer, 63, &value);
-  cr_assert(buffer.block_count == 64);
-  cr_assert(buffer.block_capacity >= 63);
+  assert(buffer.block_count == 64);
+  assert(buffer.block_capacity >= 63);
 
   nb_release(&buffer);
 }
 
-Test(insert, insert_properly_keeps_other_values) {
+void insert_properly_keeps_other_values() {
   struct nb_buffer buffer;
   uint32_t value = 0;
   uint32_t * read_value = NULL;
@@ -92,30 +91,30 @@ Test(insert, insert_properly_keeps_other_values) {
 
   nb_insert(&buffer, 0, &value);
   read_value = nb_at(&buffer, 0);
-  cr_assert(*read_value == 0);
+  assert(*read_value == 0);
 
   value = 3;
   nb_insert(&buffer, 0, &value);
   read_value = nb_at(&buffer, 1);
-  cr_assert(*read_value == 0);
+  assert(*read_value == 0);
 
   value = 33333;
   nb_insert(&buffer, 0, &value);
 
   read_value = nb_at(&buffer, 1);
-  cr_assert(*read_value == 3);
+  assert(*read_value == 3);
   read_value = nb_at(&buffer, 2);
-  cr_assert(*read_value == 0);
+  assert(*read_value == 0);
 
   value = 222;
   nb_insert(&buffer, 1, &value);
 
   read_value = nb_at(&buffer, 0);
-  cr_assert(*read_value == 33333);
+  assert(*read_value == 33333);
   read_value = nb_at(&buffer, 2);
-  cr_assert(*read_value == 3);
+  assert(*read_value == 3);
   read_value = nb_at(&buffer, 3);
-  cr_assert(*read_value == 0);
+  assert(*read_value == 0);
 
   nb_release(&buffer);
 }
