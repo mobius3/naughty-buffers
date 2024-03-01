8cb4531f97a99c45a3c62830421bca32a3bc948a
Leonardo <leonardo_freitas_ext@carrefour.com>
2024-02-29 22:05:32 -0300

    fix compilation errors

--- src/naughty-buffers/memory.h
+++ src/naughty-buffers/memory.h
@@ -3,10 +3,10 @@
 
 #include "naughty-buffers/naughty-buffers-export.h"
 
-NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_alloc(size_t memory_size, const void * context);
-NAUGHTY_BUFFERS_NO_EXPORT void nb_memory_release(void * ptr, const void * context);
-NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_realloc(void * ptr, size_t memory_size, const void * context);
-NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_copy(void * destination, const void * source, size_t size, const void * context);
-NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_move(void * destination, const void * source, size_t size, const void * context);
+NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_alloc(size_t memory_size, void * context);
+NAUGHTY_BUFFERS_NO_EXPORT void nb_memory_release(void * ptr, void * context);
+NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_realloc(void * ptr, size_t memory_size, void * context);
+NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_copy(void * destination, const void * source, size_t size, void * context);
+NAUGHTY_BUFFERS_NO_EXPORT void * nb_memory_move(void * destination, const void * source, size_t size, void * context);
 
 #endif // NAUGHTY_BUFFERS_MEMORY_H
